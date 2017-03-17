26f04d50e673b1e6f1ee3f586d6814a306d6b24a
Mark Stein <m.stein@mosst.de>
2017-03-17 16:50:15 +0100

    init spring boot with runable jar and with gradle

---
+++ spring/labor-spring-boot/src/main/java/de/mosst/labor/spring/boot/LaborSpringBootApplication.java
@@ -0,0 +1,12 @@
+package de.mosst.labor.spring.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaborSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LaborSpringBootApplication.class, args);
+	}
+}
---
+++ spring/labor-spring-boot/src/main/java/de/mosst/labor/spring/boot/MainController.java
@@ -0,0 +1,16 @@
+package de.mosst.labor.spring.boot;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class MainController {
+	
+	@RequestMapping("/")
+	@ResponseBody
+	public String rootContext(){
+		return "Start";
+	}
+
+}
